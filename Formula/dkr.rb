3a223cb89f3ca0e753ef6b50e107f2eea8d8c858
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2020-02-05 15:25:58 +0100

    chore(dkr): bumped version

--- Formula/dkr.rb
+++ Formula/dkr.rb
@@ -1,8 +1,8 @@
 class Dkr < Formula
     desc " Lightweight CLI application to make Docker more interactive and easy for developers"
     homepage "https://github.com/Roverr/dkr"
-    url "https://github.com/Roverr/dkr/releases/download/v1.0.0/dkr.darwin.amd64.tar.gz"
-    sha256 "f111318b199ec51bb46d217ebff949604612fbe134e6e87306a97278f5613fcb"
+    url "https://github.com/Roverr/dkr/releases/download/v1.0.1/dkr.darwin.amd64.tar.gz"
+    sha256 "5c1a5debc72a8fe67ad1d95272a76d057d5dd8ff6971eb0f6e8a007b819c7009"
 
     def install
         bin.install "osx/dkr"
